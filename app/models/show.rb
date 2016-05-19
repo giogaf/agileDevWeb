2fc76cec99d0eb78920d6fa00db343026d7f63b5
asdasd <nuvokay@leeching.net>
2016-05-19 20:35:24 +0000

    test y fixtures

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,9 @@
 class Show < ActiveRecord::Base
+    validates :titulo, :descripcion,presence: true
+    validates :titulo,uniqueness:true
+    validates :precio,numericality:{greater_than_or_equal_to: 0.01}
+    validates :imagen,allow_blank: true, format:{
+        with: /\.(gif|jpg|png)\z/,
+        message: 'formatos admitidos gif,jpg,png'
+    }
 end
--- test/models/show_test.rb
+++ test/models/show_test.rb
@@ -1,4 +1,59 @@
 require 'test_helper'
 
 class ShowTest < ActiveSupport::TestCase
+  
+  fixtures :shows
+  
+  test "atributos Serie no debe ser vacios" do 
+    serie= Show.new
+    assert serie.invalid?
+    assert serie.errors[:titulo].any?
+    assert serie.errors[:descripcion].any?
+    assert serie.errors[:precio].any?
+  end
+  
+  test "precio valores positivos" do
+    serie=Show.new(titulo: 'un titulo',
+                  descripcion:'una descripción',
+                  imagen: 'imagen.jpg')
+    serie.precio=-1
+    assert !serie.valid?
+    serie.precio=0
+    assert serie.invalid?    
+    serie.precio=1
+    assert serie.valid?    
+  end
+ def nueva_serie una_url
+    Show.new(
+      titulo: 'un titulo serie',
+      descripcion: 'una descripcion serie',
+      precio: 999,
+      imagen: una_url
+      )
+  end
+  
+  test 'imagen_url' do
+    urls_ok=%w{ img.jpg img.jpg img.gif img.png  
+                 https://ide.c9.io/img.png}
+    urls_bad = %w{noimg.pdf noimg.jpg/dir noimg.gif.rb }
+  
+    urls_ok.each do |url|
+      assert nueva_serie(url).valid?
+    end
+    urls_bad.each do |url|
+      assert nueva_serie(url).invalid?
+    end    
+  end  
+  
+    test "titulo único" do
+        serie= Show.new(
+            titulo: shows(:modernfamily).titulo,
+            descripcion: "otra descripción",
+            precio:99,
+            imagen: "otraimg.gif"
+            )
+    assert !serie.save
+    end
+ 
 end
+
