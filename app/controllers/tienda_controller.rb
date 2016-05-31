615ce9f91768344d9ac62e876ce30bca77c0bda6
asdasd <nuvokay@leeching.net>
2016-05-31 13:20:40 +0000

    will pagination gem

--- app/controllers/tienda_controller.rb
+++ app/controllers/tienda_controller.rb
@@ -1,7 +1,8 @@
 class TiendaController < ApplicationController
     
     def index
-     @shows = Show.all
+     #@shows = Show.all
+     @shows = Show.paginate page: params[:page], per_page: 3
      @compra = compra_actual
     end
 end
